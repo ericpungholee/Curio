1259a992421ba5ba51418fa45d81145e30909c9f
haokai-xuan <haokai.xuan2006@gmail.com>
2025-10-26 03:39:32 -0700

    Add back home button to Profile page matching CreatePost styling

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -683,6 +683,14 @@ const Profile = ({ onBackToHome, onLogout }: ProfileProps) => {
         <img src="/curio.png" alt="Curio Logo" className="logo" />
       </div>
 
+      {/* Back Button */}
+      <button className="back-button" onClick={onBackToHome}>
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+          <path d="M19 12H5M12 19l-7-7 7-7"/>
+        </svg>
+        Back to Home
+      </button>
+
       {/* Profile Container */}
       <div className="profile-container">
         <div className="profile-layout">
