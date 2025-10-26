c17fa7047aa5e77729d1c5408611976b1a4bef71
haokai-xuan <haokai.xuan2006@gmail.com>
2025-10-25 23:37:17 -0700

    Remove share button, keep like and comment buttons with numbers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -387,15 +387,6 @@ const PostCardNode = ({ data }: PostCardNodeProps) => {
           </svg>
           5
         </button>
-        <button className="post-action">
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M17 1l4 4-4 4" />
-            <path d="M3 11V9a4 4 0 0 1 4-4h14" />
-            <path d="M7 23l-4-4 4-4" />
-            <path d="M21 13v2a4 4 0 0 1-4 4H3" />
-          </svg>
-          8
-        </button>
       </div>
     </div>
   )
